4453f3958763073c76f71e58908ae001bb112193
Alaricode <antonlarichev@gmail.com>
2025-05-29 11:04:23 +0300

    Автоимпорт

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+import TestComp from "./widgets/TestComp.vue";
+
+const hello = ref("hello");
+</script>
+
 <template>
   <div>
+    {{ hello }}
+    <TestComp />
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
