b3a3ccba0b10e82f1e1a92eeb9f6dea3ecedb442
Alaricode <antonlarichev@gmail.com>
2025-06-04 15:17:59 +0300

    Страница ошибки

--- app.vue
+++ app.vue
@@ -1,4 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+throw createError({
+  statusCode: 401,
+  statusMessage: "Недоступна страница без авторизации",
+  data: {
+    userId: 1,
+  },
+});
+</script>
 
 <template>
   <div>
