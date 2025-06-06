08e0279d5ca748413c0f3eaf1204915a9e7fb21b
Alaricode <antonlarichev@gmail.com>
2025-06-06 16:04:29 +0300

    Добавление иконок

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,7 @@ function openAbout() {
 <template>
   <div>
     Index
+    <IconTwitter />
     <NuxtLink to="/about">About</NuxtLink>
     <button @click="openAbout">Перейти</button>
     <InputField variant="black" placeholder="Введите ваш email для подписки" />
