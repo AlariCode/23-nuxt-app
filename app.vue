3ef53ec1dc432cce631716ad4e6741aef97eb38c
Alaricode <antonlarichev@gmail.com>
2025-06-10 09:54:37 +0300

    Стилизация иконок

--- app.vue
+++ app.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
-import type { LayoutKey } from "#build/types/layouts";
 import "~/assets/styles/main.css";
-
-const name = ref<LayoutKey>("auth");
 </script>
 
 <template>
-  <NuxtLayout :name="name">
+  <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
 function openAbout() {
-  //   navigateTo({ path: "/about" });
-  setPageLayout("auth");
+  navigateTo({ path: "/about" });
 }
 </script>
 
 <template>
   <div>
     Index
-    <IconTwitter />
-    <Icon name="icons:twitter" />
     <NuxtLink to="/about">About</NuxtLink>
     <button @click="openAbout">Перейти</button>
     <InputField variant="black" placeholder="Введите ваш email для подписки" />
