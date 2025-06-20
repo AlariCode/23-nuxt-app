bfb4af6ad92c72202abe7eb9150a0460eaa11cee
Alaricode <antonlarichev@gmail.com>
2025-06-20 15:35:20 +0300

    Ограничения useFetch

--- pages/catalog/index.vue
+++ pages/catalog/index.vue
@@ -3,22 +3,21 @@ import type { GetCategoriesResponse } from "~/interfaces/category.interface";
 
 const config = useRuntimeConfig();
 const API_URL = config.public.apiurl;
+const input = ref("");
 
-const { data, error } = await useFetch<GetCategoriesResponse>(
-  API_URL + "/categories"
-);
-
-console.log(data.value);
-console.log(error.value);
-
-// try {
-//   const data = await $fetch<GetCategoriesResponse>(API_URL + "/categories");
-//   console.log(data);
-// } catch (e) {
-//   console.error(e);
-// }
+async function sendData() {
+  const data = await $fetch<GetCategoriesResponse>(API_URL + "/categories", {
+    method: "POST",
+    body: input,
+  });
+  console.log(data);
+}
 </script>
 
 <template>
-  <div>Catalog</div>
+  <div>
+    Catalog
+    <InputField v-model="input" variant="black" />
+    <button @click="sendData">Отправить</button>
+  </div>
 </template>
