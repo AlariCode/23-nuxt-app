71c85cb6ae81f16cc43bf81007d6ded9d406ac85
Alaricode <antonlarichev@gmail.com>
2025-06-20 15:45:32 +0300

    Refresh данных

--- pages/catalog/index.vue
+++ pages/catalog/index.vue
@@ -5,12 +5,21 @@ const config = useRuntimeConfig();
 const API_URL = config.public.apiurl;
 const input = ref("");
 
-async function sendData() {
-  const data = await $fetch<GetCategoriesResponse>(API_URL + "/categories", {
+const { data, error, refresh } = await useFetch<GetCategoriesResponse>(
+  API_URL + "/categories",
+  {
     method: "POST",
     body: input,
-  });
-  console.log(data);
+    immediate: false,
+    watch: false,
+  }
+);
+
+console.log(data.value);
+console.log(error.value);
+
+async function sendData() {
+  refresh();
 }
 </script>
 
