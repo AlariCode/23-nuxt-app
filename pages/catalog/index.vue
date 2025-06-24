a89c00beca73c14aaf86559fc2722cc8d62ca4ab
Alaricode <antonlarichev@gmail.com>
2025-06-24 16:00:33 +0300

    Упражнение - Select

--- pages/catalog/index.vue
+++ pages/catalog/index.vue
@@ -4,33 +4,36 @@ import type { GetCategoriesResponse } from "~/interfaces/category.interface";
 const config = useRuntimeConfig();
 const API_URL = config.public.apiurl;
 const input = ref("");
+const select = ref("");
 
-const { data, error, refresh } = await useAsyncData<GetCategoriesResponse>(
+await useAsyncData<GetCategoriesResponse>(
   "categories",
   () => $fetch(API_URL + "/categories"),
   {
     watch: [input],
   }
 );
-
-// $fetch - не SSR friendly, используем не на верхнем
-// useFetch - используем только на верхнем, SSR friendly
-// useAsyncData - SSR friendly, нужен если сложные вариант получения данных
-
-// API_URL + "/categories",
-
-console.log(data.value);
-console.log(error.value);
-
-async function sendData() {
-  refresh();
-}
 </script>
 
 <template>
   <div>
     Catalog
-    <InputField v-model="input" variant="black" />
-    <button @click="sendData">Отправить</button>
+    <SelectField
+      v-model="select"
+      :options="[
+        {
+          value: '',
+          label: 'Категории',
+        },
+        {
+          value: '1',
+          label: 'Первый',
+        },
+        {
+          value: '2',
+          label: 'Второй',
+        },
+      ]"
+    />
   </div>
 </template>
