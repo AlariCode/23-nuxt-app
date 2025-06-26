f294896816117d948edf5701528f71f77ad43913
Alaricode <antonlarichev@gmail.com>
2025-06-26 18:02:58 +0300

    Добавление Pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,11 +34,5 @@ export default defineNuxtConfig({
 			}
 		]
 	},
-	modules: [
-		'@nuxt/eslint',
-		'@nuxt/fonts',
-		'@nuxt/image',
-		'@nuxt/scripts',
-		'@nuxt/icon'
-	]
+	modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/image', '@nuxt/scripts', '@nuxt/icon', '@pinia/nuxt']
 })
