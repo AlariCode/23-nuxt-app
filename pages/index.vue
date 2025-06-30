ef3a7a1416944ad0fe97391bfa1b2d2496341ec3
Alaricode <antonlarichev@gmail.com>
2025-06-30 18:02:48 +0300

    useHead и useSeoMeta

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,11 @@
 function openAbout() {
   navigateTo({ path: "/about" });
 }
+useSeoMeta({
+  title: "Магазин Shoppe",
+  description: "Магазин Shoppe с ювелирными изделиями.",
+  ogDescription: "Магазин Shoppe с ювелирными изделиями.",
+});
 </script>
 
 <template>
