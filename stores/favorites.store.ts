f3e6ffcb7e87914dc0d4e690af2e16754b5752f0
Alaricode <antonlarichev@gmail.com>
2025-07-02 14:37:51 +0300

    Интеграция с frontend

--- stores/favorites.store.ts
+++ stores/favorites.store.ts
@@ -1,17 +1,8 @@
 import { defineStore } from 'pinia';
 
 export const useFavoritesStore = defineStore('favorites', () => {
+	const authStore = useAuthStore();
 	const favoriteIds = ref<number[]>([]);
-
-	function addToFavorite(id: number) {
-		if (!favoriteIds.value.includes(id)) {
-			favoriteIds.value.push(id);
-		}
-	}
-
-	function removeFromFavorite(id: number) {
-		favoriteIds.value = favoriteIds.value.filter(item => item != id);
-	}
 
 	function isFavorite(id: number) {
 		return favoriteIds.value.find(f => f == id);
@@ -23,10 +14,23 @@ export const useFavoritesStore = defineStore('favorites', () => {
 			return
 		}
 		favoriteIds.value = favoriteIds.value.filter(item => item != id);
+		if (authStore.email) {
+			save();
+		}
+	}
+
+	async function save() {
+		await $fetch<{ success: boolean }>('/api/favorites', {
+			method: 'POST',
+			body: {
+				email: authStore.email,
+				ids: favoriteIds.value
+			}
+		});
 	}
 
 
-	return { favoriteIds, addToFavorite, removeFromFavorite, toggleFavorite, isFavorite }
+	return { favoriteIds, toggleFavorite, isFavorite }
 }, {
 	persist: true
 });
